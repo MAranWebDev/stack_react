9df41a7c3c99859d87da6fa7add8757ad8e13e36
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-06 18:47:43 -0400

    chore: test

--- apps/api/src/drizzle/schemas/sample.schema.ts
+++ apps/api/src/drizzle/schemas/sample.schema.ts
@@ -8,7 +8,10 @@ import {
 
 export const sampleSchema = pgTable('sample', {
   createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
   id: uuid('id').primaryKey().defaultRandom(),
   name: varchar('name').unique().notNull(),
   isDone: boolean('is_done').notNull().default(false),
