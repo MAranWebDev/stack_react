02dc0aaeccbc9c7db2a89dc17a05c83edfc92e0f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-09 17:57:49 -0300

    chore: test

--- apps/api/src/drizzle/migrations/0000_amused_peter_parker.sql
+++ apps/api/src/drizzle/migrations/0000_amused_peter_parker.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS "sample" (
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar NOT NULL,
+	"is_done" boolean DEFAULT false NOT NULL,
+	CONSTRAINT "sample_name_unique" UNIQUE("name")
+);
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user-profiles" (
 	"created_at" timestamp,
 	"updated_at" timestamp,
