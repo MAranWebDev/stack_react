9a67a3da4137c3b7e3a531b05f25b563570e90dd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-11 18:57:32 -0300

    chore: test

--- apps/web/src/libs/trpc.ts
+++ apps/web/src/libs/trpc.ts
@@ -1,10 +1,5 @@
 import { createTRPCReact } from '@trpc/react-query';
-import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import type { TrpcRouterType } from '@workspace/api';
 
 // Trpc hooks
 export const trpc = createTRPCReact<TrpcRouterType>();
-
-// Exported types
-export type RouterInputType = inferRouterInputs<TrpcRouterType>;
-export type RouterOutputType = inferRouterOutputs<TrpcRouterType>;
