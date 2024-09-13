9e2ec87b1ff45fde95681b3b52b3aba0c185308d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-13 16:30:15 -0300

    chore: test

--- apps/api/src/exports.ts
+++ apps/api/src/exports.ts
@@ -2,5 +2,5 @@ export type {
   TrpcRouterInputType,
   TrpcRouterOutputType,
   TrpcRouterType,
-} from './app/router/trpc-router';
-export * from './zod/schemas';
+} from './app/trpc-router';
+export * from './libs/zod/schemas';
