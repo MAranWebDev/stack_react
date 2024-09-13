dbfa5142fc6f5ada4397107021572164b0b21841
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-13 16:29:06 -0300

    chore: test

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 import { DATABASE_URL } from './src/config/env';
-import { MIGRATIONS_FOLDER } from './src/drizzle/constants';
+import {
+  MIGRATIONS_FOLDER,
+  SCHEMAS_FOLDER,
+} from './src/libs/drizzle/constants';
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/drizzle/schemas/*', // For drizzle generate
+  schema: SCHEMAS_FOLDER, // For drizzle generate
   out: MIGRATIONS_FOLDER, // For drizzle generate
   dbCredentials: { url: DATABASE_URL }, // For drizzle studio,
   verbose: true, // print all queries that will be executed
