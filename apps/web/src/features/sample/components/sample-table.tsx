7c48fab2753bdf0148f60614e64fafd7bd7a5235
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-13 16:50:51 -0300

    chore: test

---
+++ apps/web/src/features/sample/components/sample-table.tsx
@@ -0,0 +1,48 @@
+import { useReadSampleContext } from '@/features/sample/context';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { DeleteDialog } from './delete-dialog';
+import { EditDialog } from './edit-dialog';
+
+const STATUS = {
+  CLOSE: 'cerrado',
+  OPEN: 'abierto',
+} as const;
+
+export const SampleTable = () => {
+  const { results } = useReadSampleContext();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Estado</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {results?.map(({ id, name, isDone }) => (
+            <TableRow key={id} hover>
+              <TableCell>{id}</TableCell>
+              <TableCell>{name}</TableCell>
+              <TableCell>{isDone ? STATUS.CLOSE : STATUS.OPEN}</TableCell>
+              <TableCell>
+                <EditDialog id={id} />
+              </TableCell>
+              <TableCell>
+                <DeleteDialog id={id} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
