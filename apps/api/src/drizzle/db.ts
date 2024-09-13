2c2fb6c997c5d12b7751c23cfcfdbf61b4d97299
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-13 16:28:11 -0300

    chore: test

--- apps/api/src/drizzle/db.ts
+++
@@ -1,7 +0,0 @@
-import { DATABASE_URL } from '@/config/env';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schemas';
-
-const connection = postgres(DATABASE_URL);
-export const db = drizzle(connection, { schema });
