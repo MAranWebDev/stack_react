9b1b6175e3d4274d587c5f8eecf1b8d7a635cec6
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-13 14:50:21 -0300

    chore: test

--- apps/web/src/app/router/react-router.tsx
+++ apps/web/src/app/router/react-router.tsx
@@ -1,7 +1,7 @@
-import { ErrorPage } from '@/app/routes/ErrorPage';
-import { HomePage } from '@/app/routes/HomePage';
-import { SamplePage } from '@/app/routes/SamplePage';
-import { UnauthorizedPage } from '@/app/routes/UnauthorizedPage';
+import { LandingRoute } from '@/app/routes/landing.route';
+import { NotFoundRoute } from '@/app/routes/not-found.route';
+import { SampleRoute } from '@/app/routes/sample.route';
+import { UnauthorizedRoute } from '@/app/routes/unauthorized.route';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ROUTES } from './constants';
 
@@ -9,11 +9,11 @@ import { ROUTES } from './constants';
 const router = createBrowserRouter([
   {
     path: ROUTES.HOME,
-    errorElement: <ErrorPage />, // Global error page
-    element: <HomePage />,
+    errorElement: <NotFoundRoute />, // Global error page
+    element: <LandingRoute />,
   },
-  { path: ROUTES.SAMPLE, element: <SamplePage /> },
-  { path: ROUTES.UNAUTHORIZED, element: <UnauthorizedPage /> },
+  { path: ROUTES.SAMPLE, element: <SampleRoute /> },
+  { path: ROUTES.UNAUTHORIZED, element: <UnauthorizedRoute /> },
 ]);
 
 export const ReactRouter = () => {
