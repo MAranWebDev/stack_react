02c5c2756db6c9be3316c6986731439edca2aa96
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-14 20:14:36 -0300

    chore: test

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,18 +1,25 @@
 import eslint from '@eslint/js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import checkFile from 'eslint-plugin-check-file';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-// .config allows types for the config file
+// In typescript-eslint, .config allows types for the config file
 export default tseslint.config(
   // Ignore
   { ignores: ['dist'] },
 
   //Plugins
-  { plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh } },
+  {
+    plugins: {
+      ['react-hooks']: reactHooks,
+      ['react-refresh']: reactRefresh,
+      ['check-file']: checkFile,
+    },
+  },
 
   // Extends
   ...pluginQuery.configs['flat/recommended'],
@@ -24,20 +31,36 @@ export default tseslint.config(
   // Base config
   {
     files: ['**/*.{ts,tsx}'],
-    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
 
     rules: {
-      // Vite default rules
+      // react-hooks
       ...reactHooks.configs.recommended.rules,
+
+      // react-refresh
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
 
-      // Custom rules
+      // native
       'no-restricted-imports': [
         'error',
         { patterns: ['src', '../**', '@/*/*/*/*', './*/*/*/*'] },
+      ],
+
+      // check-file
+      'check-file/filename-naming-convention': [
+        'error',
+        { '**/*.{ts,tsx}': 'KEBAB_CASE' },
+        { ignoreMiddleExtensions: true },
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        { 'src/**/!(__tests__)': 'KEBAB_CASE' },
       ],
     },
   },
