c4e24135dfda78bf97db3ab9f19d156092eaa56b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-17 11:34:06 -0300

    chore: test

--- apps/web/src/libs/zustand/stores/sample.store.ts
+++ apps/web/src/libs/zustand/stores/sample.store.ts
@@ -2,9 +2,12 @@ import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 
 // Types
+type BearsType = number;
+
 interface StateType {
-  bears: number;
+  bears: BearsType;
   addBear: () => void;
+  addBearBy: (by: BearsType) => void;
   removeBears: () => void;
 }
 
@@ -17,9 +20,14 @@ export const useSampleStore = create<StateType>()(
       (set) => ({
         bears: 0,
         addBear: () => set((state) => ({ bears: state.bears + 1 })),
+        addBearBy: (by: number) =>
+          set((state) => ({ bears: state.bears + by })),
         removeBears: () => set({ bears: 0 }),
       }),
-      { name: STORE_NAME, partialize: (state) => ({ bears: state }) },
+      {
+        name: STORE_NAME,
+        partialize: (state) => ({ bears: state.bears }),
+      },
     ),
   ),
 );
