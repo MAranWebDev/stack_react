21ae958d1a7262b702d6de4072e15ff3567493c0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-17 11:13:10 -0300

    chore: test

--- apps/web/src/features/sample/components/index.tsx
+++ apps/web/src/features/sample/components/index.tsx
@@ -4,11 +4,13 @@ import { CreateBox } from './create-box';
 import { FilterBox } from './filter-box';
 import { SamplePagination } from './sample-pagination';
 import { SampleTable } from './sample-table';
+import { SampleZustand } from './sample-zustand';
 
 export const Sample = () => {
   return (
     <SampleProvider>
       <Stack sx={{ minWidth: 650 }} spacing={1}>
+        <SampleZustand />
         <CreateBox />
         <FilterBox />
         <SampleTable />
