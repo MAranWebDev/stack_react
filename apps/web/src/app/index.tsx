f382f72e45184269b7e6183555e4a0aec3983a32
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-20 16:03:30 -0300

    chore: test

--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -1,9 +1,11 @@
+import { StatusBar } from '@/components/status-bar';
 import { AppProvider } from './provider';
 import { ReactRouter } from './react-router';
 
 export const App = () => {
   return (
     <AppProvider>
+      <StatusBar />
       <ReactRouter />
     </AppProvider>
   );
