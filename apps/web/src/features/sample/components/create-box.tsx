a5aa2379c340fb9973d72dbaa63ac18f4763ae75
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-22 15:00:55 -0300

    chore: test

--- apps/web/src/features/sample/components/create-box.tsx
+++ apps/web/src/features/sample/components/create-box.tsx
@@ -1,4 +1,5 @@
 import { trpc } from '@/libs/trpc/hooks';
+import { useStatusStore } from '@/libs/zustand/stores';
 import { zodResolver } from '@hookform/resolvers/zod';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
@@ -11,18 +12,43 @@ import { useForm } from 'react-hook-form';
 type SchemaType = SampleZodType['createInput'];
 
 export const CreateBox = () => {
-  const { register, handleSubmit } = useForm<SchemaType>({
+  // react-hook-form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<SchemaType>({
     resolver: zodResolver(sampleZod.createInput),
   });
 
+  // zustand
+  const updateStatus = useStatusStore((state) => state.updateStatus);
+  const isPending = useStatusStore((state) => state.isPending);
+  const isError = useStatusStore((state) => state.isError);
+
+  // trpc
   const utils = trpc.useUtils();
   const sampleCreate = trpc.sample.create.useMutation({
+    onMutate() {
+      updateStatus({ status: 'pending' });
+    },
     onSuccess() {
       utils.sample.getAll.invalidate();
+      updateStatus({ status: 'success' });
+    },
+    onError({ message }) {
+      updateStatus({ status: 'error', errorMessage: message });
     },
   });
 
   const onSubmit = ({ name }: SchemaType) => sampleCreate.mutate({ name });
+
+  const watchedFields = watch(['name']);
+  const isFormEmpty = Object.values(watchedFields).some(
+    (value) => value === '',
+  );
+  const isAddButtonDisabled = isFormEmpty || isPending || isError;
 
   return (
     <Paper
@@ -30,9 +56,16 @@ export const CreateBox = () => {
       component="form"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <TextField label="Nombre" variant="outlined" {...register('name')} />
+      <TextField
+        required
+        label="Nombre"
+        variant="outlined"
+        error={!!errors.name}
+        helperText={errors.name?.message}
+        {...register('name')}
+      />
 
-      <Button type="submit" variant="outlined">
+      <Button type="submit" variant="outlined" disabled={isAddButtonDisabled}>
         <AddIcon />
       </Button>
     </Paper>
