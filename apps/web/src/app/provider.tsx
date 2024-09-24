a9f4bc1f917b836b5a4bb4ee28810fd568e03bd4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-24 16:36:47 -0300

    chore: test

--- apps/web/src/app/provider.tsx
+++ apps/web/src/app/provider.tsx
@@ -1,14 +1,11 @@
 import { MuiThemeProvider } from '@/libs/mui/mui-theme.provider';
-import { NotistackProvider } from '@/libs/notistack/notistack.provider';
 import { TrpcProvider } from '@/libs/trpc/trpc.provider';
 import { PropsWithChildren } from 'react';
 
 export const AppProvider = ({ children }: PropsWithChildren) => {
   return (
     <TrpcProvider>
-      <NotistackProvider>
-        <MuiThemeProvider>{children}</MuiThemeProvider>
-      </NotistackProvider>
+      <MuiThemeProvider>{children}</MuiThemeProvider>
     </TrpcProvider>
   );
 };
