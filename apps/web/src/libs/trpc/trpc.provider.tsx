a076d78670c19c324055099710b8493482514f91
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-24 16:33:46 -0300

    chore: test

--- apps/web/src/libs/trpc/trpc.provider.tsx
+++ apps/web/src/libs/trpc/trpc.provider.tsx
@@ -1,23 +1,55 @@
 import { VITE_TRPC_URL } from '@/config/env';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useNotification } from '@/libs/notistack/hooks';
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { httpBatchLink } from '@trpc/client';
 import { PropsWithChildren, useState } from 'react';
 import { trpc } from './hooks';
 
 export const TrpcProvider = ({ children }: PropsWithChildren) => {
+  // trpc
   const [trpcClient] = useState(() =>
     trpc.createClient({ links: [httpBatchLink({ url: VITE_TRPC_URL })] }),
   );
-  const [reactQueryClient] = useState(() => new QueryClient());
+
+  const { show } = useNotification();
+
+  // react-query
+  const [reactQueryClient] = useState(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError({ message }) {
+            show({ variant: 'error', text: message });
+          },
+          onSuccess() {
+            show({ variant: 'success' });
+          },
+        }),
+        mutationCache: new MutationCache({
+          onError({ message }) {
+            show({ variant: 'error', text: message });
+          },
+          onSuccess() {
+            show({ variant: 'success' });
+          },
+        }),
+      }),
+  );
 
   return (
-    // Trpc
+    // trpc
     <trpc.Provider client={trpcClient} queryClient={reactQueryClient}>
-      {/* React query */}
+      {/* react-query */}
       <QueryClientProvider client={reactQueryClient}>
         {children}
-        {/* React query devtools */}
+
+        {/* react-query-devtools */}
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </trpc.Provider>
