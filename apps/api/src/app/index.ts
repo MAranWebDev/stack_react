f4955e63e8c8685bef3e091f4c2dbac16dbdd150
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-09-27 16:02:58 -0300

    chore: test

--- apps/api/src/app/index.ts
+++ apps/api/src/app/index.ts
@@ -1,9 +1,8 @@
-import { SERVER_PORT } from '@/config/env';
+import { NODE_ENV, SERVER_PORT } from '@/config/env';
 import { createContext } from '@/libs/trpc/utils';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import cors from 'cors';
 import express from 'express';
-import { expressHandler } from 'trpc-playground/handlers/express';
 import { trpcRouter } from './trpc-router';
 
 // Constants
@@ -25,14 +24,17 @@ const runApp = async () => {
   );
 
   // trpc-playground
-  app.use(
-    ENDPOINTS.TRPC_PLAYGROUND,
-    await expressHandler({
+  if (NODE_ENV !== 'production') {
+    const { expressHandler } = await import('trpc-playground/handlers/express');
+
+    const handler = await expressHandler({
       trpcApiEndpoint: ENDPOINTS.TRPC_API,
       playgroundEndpoint: ENDPOINTS.TRPC_PLAYGROUND,
       router: trpcRouter,
-    }),
-  );
+    });
+
+    app.use(ENDPOINTS.TRPC_PLAYGROUND, handler);
+  }
 
   app.listen(SERVER_PORT, () => {
     console.log(`Server is running on port: ${SERVER_PORT}`);
