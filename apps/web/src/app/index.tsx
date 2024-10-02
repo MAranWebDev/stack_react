34bf7db1d819a8777da98c98d8d562a2e5c6bfd1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-02 17:20:53 -0300

    chore: test

--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -1,14 +1,9 @@
-import { LanguageSwitch } from '@/components/language-switch';
-import { LoadingBar } from '@/components/loading-bar';
 import { AppProvider } from './provider';
 import { ReactRouter } from './react-router';
 
 export const App = () => {
   return (
     <AppProvider>
-      <LanguageSwitch />
-      <LoadingBar />
-
       <ReactRouter />
     </AppProvider>
   );
