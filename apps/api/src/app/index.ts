9b13b640eb827a2153fc59effc1191971663eedd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-02 19:48:00 -0300

    chore: test

--- apps/api/src/app/index.ts
+++ apps/api/src/app/index.ts
@@ -1,4 +1,4 @@
-import { NODE_ENV, SERVER_PORT } from '@/config/env';
+import { SERVER_PORT } from '@/config/env';
 import { createContext } from '@/libs/trpc/utils';
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import cors from 'cors';
@@ -8,37 +8,19 @@ import { trpcRouter } from './trpc-router';
 // Constants
 const ENDPOINTS = {
   TRPC_API: '/trpc',
-  TRPC_PLAYGROUND: '/trpc-playground',
 } as const;
 
-const runApp = async () => {
-  const app = express();
+const app = express();
 
-  // "cors"
-  app.use(cors());
+// "cors"
+app.use(cors());
 
-  // "trpc"
-  app.use(
-    ENDPOINTS.TRPC_API,
-    createExpressMiddleware({ router: trpcRouter, createContext }),
-  );
+// "trpc"
+app.use(
+  ENDPOINTS.TRPC_API,
+  createExpressMiddleware({ router: trpcRouter, createContext }),
+);
 
-  // "trpc-playground"
-  if (NODE_ENV !== 'production') {
-    const { expressHandler } = await import('trpc-playground/handlers/express');
-
-    const handler = await expressHandler({
-      trpcApiEndpoint: ENDPOINTS.TRPC_API,
-      playgroundEndpoint: ENDPOINTS.TRPC_PLAYGROUND,
-      router: trpcRouter,
-    });
-
-    app.use(ENDPOINTS.TRPC_PLAYGROUND, handler);
-  }
-
-  app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on port: ${SERVER_PORT}`);
-  });
-};
-
-runApp();
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port: ${SERVER_PORT}`);
+});
--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,5 +1,4 @@
 import 'dotenv/config';
 
-export const NODE_ENV = process.env.NODE_ENV as string;
 export const SERVER_PORT = process.env.SERVER_PORT as string;
 export const DATABASE_URL = process.env.DATABASE_URL as string;
