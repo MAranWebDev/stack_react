a4ee6a00b62654e82861d7971691c2a0ab97683b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-03 18:17:10 -0300

    chore: test

---
+++ apps/web/src/@types/i18next-resources.d.ts
@@ -0,0 +1,12 @@
+interface Resources {
+  "en": {
+    "closed": "closed",
+    "english": "english",
+    "language": "language",
+    "noData": "no data",
+    "open": "open",
+    "spanish": "spanish"
+  }
+}
+
+export default Resources;
