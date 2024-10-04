ed9753b839444bd01bbb34c9b8f34a5adf4b4a16
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-04 09:16:30 -0300

    chore: test

---
+++ apps/web/src/libs/i18next/i18n.ts
@@ -0,0 +1,33 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend, { HttpBackendOptions } from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+// Constants
+export const DEFAULT_NS = 'translation';
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init<HttpBackendOptions>({
+    // Http backend options
+    backend: {},
+
+    // Other i18next options
+    debug: false,
+    fallbackLng: 'en',
+    defaultNS: DEFAULT_NS,
+    interpolation: {
+      escapeValue: false, // Not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
