98cb6ce5ca3eb2769a33624e850db09bb80cf34b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-04 18:22:20 -0300

    chore: test

--- apps/web/src/constants/routes.ts
+++ apps/web/src/constants/routes.ts
@@ -1,7 +1,9 @@
-export type ROUTES = (typeof ROUTES)[keyof typeof ROUTES];
-
+// Constants
 export const ROUTES = {
   HOME: '/',
   SAMPLE: '/sample',
   UNAUTHORIZED: '/unauthorized',
 } as const;
+
+// Exported types
+export type ROUTES = (typeof ROUTES)[keyof typeof ROUTES];
