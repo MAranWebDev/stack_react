bfdff461c5cf230fe5b50f86b5ade56e4772283f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-05 13:32:09 -0300

    chore: test

--- apps/web/src/libs/i18next/constants.ts
+++ apps/web/src/libs/i18next/constants.ts
@@ -1,1 +1,10 @@
+// Constants
 export const DEFAULT_NS = 'translation';
+
+export const LANGUAGES = {
+  ENGLISH: 'en',
+  SPANISH: 'es',
+} as const;
+
+// Exported types
+export type LANGUAGES = (typeof LANGUAGES)[keyof typeof LANGUAGES];
