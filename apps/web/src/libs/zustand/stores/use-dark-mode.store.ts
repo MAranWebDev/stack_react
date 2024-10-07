6ca23d86133559b9030d2ecf3b1a6ee835e45270
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-07 16:34:54 -0300

    chore: test

--- apps/web/src/libs/zustand/stores/use-dark-mode.store.ts
+++ apps/web/src/libs/zustand/stores/use-dark-mode.store.ts
@@ -1,25 +1,40 @@
+import { MODES } from '@/libs/zustand/constants';
 import { applyMiddlewares } from '@/libs/zustand/utils';
 import { create } from 'zustand';
 
 // Types
 interface Store {
   isDarkMode: boolean;
-  toggleDarkMode: () => void;
+  changeMode: (mode: MODES) => void;
 }
 
 // Constants
 const PERSIST_STORE_NAME = 'darkModeStore';
 const PERSIST_KEYS = ['isDarkMode'] as const;
 
+// Methods
+const getIsDarkMode = (mode: MODES) => {
+  if (mode === MODES.DARK) return true;
+  if (mode === MODES.LIGHT) return false;
+
+  const systemIsDark = window.matchMedia(
+    '(prefers-color-scheme: dark)',
+  ).matches;
+  return systemIsDark;
+};
+
+// Initial values
+const initialIsDarkMode = getIsDarkMode(MODES.SYSTEM);
+
 export const useDarkModeStore = create<Store>()(
   applyMiddlewares({
     persistStoreName: PERSIST_STORE_NAME,
     persistKeys: PERSIST_KEYS,
     store: (set) => ({
-      isDarkMode: false,
-      toggleDarkMode: () =>
+      isDarkMode: initialIsDarkMode,
+      changeMode: (mode: MODES) =>
         set((state) => {
-          state.isDarkMode = !state.isDarkMode;
+          state.isDarkMode = getIsDarkMode(mode);
         }),
     }),
   }),
