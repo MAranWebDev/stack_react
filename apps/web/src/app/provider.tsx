172d4080b2d0d3d0213b69740e1716aa804cd27d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-07 16:36:37 -0300

    chore: test

--- apps/web/src/app/provider.tsx
+++ apps/web/src/app/provider.tsx
@@ -1,15 +1,18 @@
-import { I18nextSuspenseProvider } from '@/libs/i18next/i18next-suspense.provider';
+import { CenteredSpinner } from '@/components/ui/spinner';
+import { I18nextSSRProvider } from '@/libs/i18next/i18next-ssr.provider';
 import { MuiThemeProvider } from '@/libs/mui/mui-theme.provider';
 import { TrpcProvider } from '@/libs/trpc/trpc.provider';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, Suspense } from 'react';
 
 export const AppProvider = ({ children }: PropsWithChildren) => {
   return (
-    // "mui" must be first to trigger functionalities in other providers
-    <MuiThemeProvider>
-      <TrpcProvider>
-        <I18nextSuspenseProvider>{children}</I18nextSuspenseProvider>
-      </TrpcProvider>
-    </MuiThemeProvider>
+    <Suspense fallback={<CenteredSpinner />}>
+      {/* // "mui" must be first to trigger functionalities in other providers */}
+      <MuiThemeProvider>
+        <TrpcProvider>
+          <I18nextSSRProvider>{children}</I18nextSSRProvider>
+        </TrpcProvider>
+      </MuiThemeProvider>
+    </Suspense>
   );
 };
