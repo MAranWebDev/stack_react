d0d5956b671e1bd1b1a0a1c9e2bb6f1aff2fb44a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-07 17:44:08 -0300

    chore: test

--- apps/web/src/app/routes/sample.route.tsx
+++ apps/web/src/app/routes/sample.route.tsx
@@ -1,15 +1,12 @@
 import { MainLayout } from '@/components/layouts/main-layout';
 import { Sample } from '@/features/sample/components';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
 export const SampleRoute = () => {
   return (
     <MainLayout>
       <Box component="section">
-        <Typography sx={{ fontWeight: 'medium' }} component="h1" variant="h4">
-          Sample
-        </Typography>
+        <h1>Sample Route</h1>
       </Box>
 
       <Box component="section">
