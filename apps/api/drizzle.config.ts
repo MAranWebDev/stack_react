04c6cb8d0d0dfc9cf2ccad80b01713104d341eb5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-07 19:14:20 -0300

    chore: test

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'drizzle-kit';
-import { DATABASE_URL } from './src/config/env';
 import {
   MIGRATIONS_FOLDER,
   SCHEMAS_FOLDER,
@@ -9,7 +8,6 @@ export default defineConfig({
   dialect: 'postgresql',
   schema: SCHEMAS_FOLDER, // For drizzle generate
   out: MIGRATIONS_FOLDER, // For drizzle generate
-  dbCredentials: { url: DATABASE_URL }, // For drizzle studio,
   verbose: true, // Print all queries that will be executed
   strict: true, // Always ask for an approve before pushing the schema
 });
