8a5918d8b47e6d809d9e19ed6a1baa27f8b329a4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-07 18:20:16 -0300

    chore: test

---
+++ apps/web/src/libs/i18next/locales/index.ts
@@ -0,0 +1,6 @@
+// Imported json locales
+import enLocale from './en.json';
+import esLocale from './es.json';
+
+// Exported locales
+export { enLocale, esLocale };
--- apps/web/src/libs/zustand/constants/modes.ts
+++
@@ -1,9 +0,0 @@
-// Constants
-export const MODES = {
-  DARK: 'dark',
-  LIGHT: 'light',
-  SYSTEM: 'system',
-} as const;
-
-// Exported types
-export type MODES = (typeof MODES)[keyof typeof MODES];
