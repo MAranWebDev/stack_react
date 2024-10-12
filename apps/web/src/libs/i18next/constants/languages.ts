7a3a065a02b89669a7ba3714aa5133016b10b395
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-12 09:25:10 -0300

    chore: test

--- apps/web/src/libs/i18next/constants/languages.ts
+++ apps/web/src/libs/i18next/constants/languages.ts
@@ -4,5 +4,8 @@ export const LANGUAGES = {
   ES: 'es', // Spanish
 } as const;
 
+export const LANGUAGE_VALUES = Object.values(LANGUAGES);
+
 // Exported types
 export type LANGUAGES = (typeof LANGUAGES)[keyof typeof LANGUAGES];
+export type LanguageValues = typeof LANGUAGE_VALUES;
