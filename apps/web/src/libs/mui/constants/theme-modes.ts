6b4b93c1a2951e4e78a3c91bee7b6c1f73f8c8fa
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-12 09:26:38 -0300

    chore: test

--- apps/web/src/libs/mui/constants/theme-modes.ts
+++ apps/web/src/libs/mui/constants/theme-modes.ts
@@ -5,5 +5,8 @@ export const THEME_MODES = {
   SYSTEM: 'system',
 } as const;
 
+export const THEME_MODE_VALUES = Object.values(THEME_MODES);
+
 // Exported types
 export type THEME_MODES = (typeof THEME_MODES)[keyof typeof THEME_MODES];
+export type ThemeModeValues = typeof THEME_MODE_VALUES;
