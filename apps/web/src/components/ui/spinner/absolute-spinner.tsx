5f6a58e668092fbf95c308dfcc4614dc2f89aee4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-12 19:42:20 -0300

    chore: test

--- apps/web/src/components/ui/spinner/absolute-spinner.tsx
+++
@@ -1,9 +0,0 @@
-import CircularProgress from '@mui/material/CircularProgress';
-
-export const AbsoluteSpinner = () => {
-  return (
-    <CircularProgress
-      sx={{ position: 'absolute', zIndex: 'tooltip', top: 10, right: 22 }}
-    />
-  );
-};
