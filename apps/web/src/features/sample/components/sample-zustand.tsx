61f22d669de9006d1b5473a7a26dd37d586e92a6
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-14 20:50:17 -0300

    chore: test

--- apps/web/src/features/sample/components/sample-zustand.tsx
+++ apps/web/src/features/sample/components/sample-zustand.tsx
@@ -7,22 +7,22 @@ import Typography from '@mui/material/Typography';
 import { useTranslation } from 'react-i18next';
 
 // Constants
-const MULTIPLE_BEARS = 3;
+const ADD_BY = 3;
 
 export const SampleZustand = () => {
   // "zustand"
-  const bears = useSampleStore((state) => state.bears);
-  const addBear = useSampleStore((state) => state.addBear);
-  const addBearBy = useSampleStore((state) => state.addBearBy);
-  const removeBears = useSampleStore((state) => state.removeBears);
+  const amount = useSampleStore((state) => state.amount);
+  const add = useSampleStore((state) => state.add);
+  const addBy = useSampleStore((state) => state.addBy);
+  const reset = useSampleStore((state) => state.reset);
 
   // "react-i18next"
   const { t } = useTranslation();
 
   // Methods
-  const handleAddOneBear = () => addBear();
-  const handleAddMultipleBears = () => addBearBy(MULTIPLE_BEARS);
-  const handleResetBears = () => removeBears();
+  const handleAdd = () => add();
+  const handleAddBy = () => addBy(ADD_BY);
+  const handleReset = () => reset();
 
   return (
     <Box
@@ -31,15 +31,15 @@ export const SampleZustand = () => {
       gap={2}
     >
       <Typography sx={{ fontWeight: 'medium' }}>
-        {t('bears')}: {bears}
+        {t('amount')}: {amount}
       </Typography>
-      <Button variant="outlined" onClick={handleAddOneBear}>
+      <Button variant="outlined" onClick={handleAdd}>
         +1
       </Button>
-      <Button variant="outlined" onClick={handleAddMultipleBears}>
-        +{MULTIPLE_BEARS}
+      <Button variant="outlined" onClick={handleAddBy}>
+        +{ADD_BY}
       </Button>
-      <Button variant="outlined" onClick={handleResetBears}>
+      <Button variant="outlined" onClick={handleReset}>
         <CleaningServicesIcon />
       </Button>
     </Box>
