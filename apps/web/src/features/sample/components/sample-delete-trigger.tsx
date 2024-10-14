0ce8b0eace558c81ba1104151f2f791f9a003557
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-14 18:48:41 -0300

    chore: test

---
+++ apps/web/src/features/sample/components/sample-delete-trigger.tsx
@@ -0,0 +1,39 @@
+import { ConfirmationDialog } from '@/components/ui/dialog';
+import { useTrpcSample } from '@/features/sample/hooks';
+import ClearIcon from '@mui/icons-material/Clear';
+import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// Types
+interface Props {
+  id: string;
+}
+
+export const SampleDeleteTrigger = ({ id }: Props) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { deleteSample } = useTrpcSample();
+
+  // "react-i18next"
+  const { t } = useTranslation();
+
+  // Methods
+  const handleOpenDialog = () => setIsDialogOpen(true);
+  const handleCloseDialog = () => setIsDialogOpen(false);
+  const handleAccept = () => deleteSample({ id });
+
+  return (
+    <>
+      <IconButton aria-label="delete" onClick={handleOpenDialog}>
+        <ClearIcon />
+      </IconButton>
+
+      <ConfirmationDialog
+        open={isDialogOpen}
+        textWarning={t('messages.warningDelete')}
+        onClose={handleCloseDialog}
+        onAccept={handleAccept}
+      />
+    </>
+  );
+};
