47db6c194a009cd986d05ce2aa88b47eebd2c3a4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-14 15:03:43 -0300

    chore: test

--- apps/web/src/features/sample/components/index.tsx
+++ apps/web/src/features/sample/components/index.tsx
@@ -1,7 +1,7 @@
 import { SampleProvider } from '@/features/sample/context';
 import Stack from '@mui/material/Stack';
-import { CreateBox } from './create-box';
 import { FilterBox } from './filter-box';
+import { SampleCreateForm } from './sample-create-form';
 import { SamplePagination } from './sample-pagination';
 import { SampleTable } from './sample-table';
 import { SampleZustand } from './sample-zustand';
@@ -10,7 +10,7 @@ export const Sample = () => {
   return (
     <SampleProvider>
       <Stack sx={{ minWidth: 650 }} spacing={1}>
-        <CreateBox />
+        <SampleCreateForm />
         <FilterBox />
         <SampleTable />
         <SamplePagination />
--- apps/web/src/features/sample/hooks/index.ts
+++ apps/web/src/features/sample/hooks/index.ts
@@ -1,2 +1,3 @@
+export * from './use-sample-create';
 export * from './use-sample-delete';
 export * from './use-sample-update';
