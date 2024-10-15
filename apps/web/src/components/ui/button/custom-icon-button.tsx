bfd54ef7c88628073f8321f6584f91f6140f7eba
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-15 15:26:32 -0300

    chore: test

--- apps/web/src/components/ui/button/custom-icon-button.tsx
+++ apps/web/src/components/ui/button/custom-icon-button.tsx
@@ -11,12 +11,8 @@ export const CustomIconButton = ({
 }: PropsWithChildren<Props>) => {
   return (
     <IconButton
-      sx={{
-        border: 1,
-        borderColor: 'text.disabled',
-        color: 'inherit',
-      }}
-      aria-label="button"
+      sx={{ border: 1, borderColor: 'text.disabled', color: 'primary.main' }}
+      aria-label="icon button"
       onClick={onClick}
     >
       {children}
