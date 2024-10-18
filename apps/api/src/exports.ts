0a76b3117e0219788908a1b7b65fb4e36a1101c1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-18 16:27:13 -0300

    chore: test

--- apps/api/src/exports.ts
+++ apps/api/src/exports.ts
@@ -4,3 +4,4 @@ export type {
   TrpcRouterOutput,
 } from './app/trpc-router';
 export * from './libs/zod/schemas';
+export * from './libs/zod/zod-i18n-map';
--- apps/web/src/features/sample/components/sample-create-trigger.tsx
+++ apps/web/src/features/sample/components/sample-create-trigger.tsx
@@ -1,17 +1,18 @@
+import { ConfirmationDialog } from '@/components/ui/dialog';
 import { useTrpcSample } from '@/features/sample/hooks';
 import { zodResolver } from '@hookform/resolvers/zod';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-import { SampleZod, sampleZod } from '@workspace/api';
+import { SampleZodCreateInput, sampleZodCreateInput } from '@workspace/api';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-// Types
-type Schema = SampleZod['createInput'];
-
 export const SampleCreateTrigger = () => {
+  const [formData, setFormData] = useState<SampleZodCreateInput | null>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const { createSample } = useTrpcSample();
 
   // "react-i18next"
@@ -21,34 +22,63 @@ export const SampleCreateTrigger = () => {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
-  } = useForm<Schema>({
-    resolver: zodResolver(sampleZod.createInput),
+  } = useForm<SampleZodCreateInput>({
+    resolver: zodResolver(sampleZodCreateInput),
   });
 
   // Methods
-  const onSubmit = ({ name }: Schema) => createSample({ name });
+  const handleOpenDialog = () => setIsDialogOpen(true);
+
+  const onSubmit = (newFormData: SampleZodCreateInput) => {
+    setFormData(newFormData);
+    handleOpenDialog();
+  };
+
+  const handleCloseDialog = () => {
+    setFormData(null);
+    setIsDialogOpen(false);
+  };
+
+  const handleAccept = () => {
+    if (formData) {
+      createSample.mutate(formData, {
+        onSuccess() {
+          reset();
+        },
+      });
+    }
+    handleCloseDialog();
+  };
 
   return (
-    <Paper
-      sx={{ display: 'flex', gap: 1, p: 2 }}
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <TextField
-        required
-        size="medium"
-        label={t('name')}
-        variant="outlined"
-        autoComplete="off"
-        error={!!errors.name}
-        helperText={errors.name?.message}
-        {...register('name')}
-      />
+    <>
+      <Paper
+        sx={{ display: 'flex', gap: 1, p: 2 }}
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextField
+          required
+          label={t('name')}
+          variant="outlined"
+          autoComplete="off"
+          error={!!errors.name}
+          helperText={errors.name?.message}
+          {...register('name')}
+        />
+        <Button type="submit" variant="outlined">
+          <AddIcon />
+        </Button>
+      </Paper>
 
-      <Button type="submit" variant="outlined">
-        <AddIcon />
-      </Button>
-    </Paper>
+      <ConfirmationDialog
+        open={isDialogOpen}
+        textWarning={t('messages.warningCreate')}
+        onClose={handleCloseDialog}
+        onAccept={handleAccept}
+      />
+    </>
   );
 };
