5c8dc74e5d738510fb75814f8f5034fa0c7daa80
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-22 19:05:35 -0300

    chore: test

--- apps/web/src/features/sample/components/index.tsx
+++ apps/web/src/features/sample/components/index.tsx
@@ -1,50 +1,48 @@
 import { SampleProvider } from '@/features/sample/context';
+import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { MouseEvent, useState } from 'react';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import { SyntheticEvent, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SampleCounter } from './sample-counter';
-import { SampleCreateTrigger } from './sample-create-trigger';
-import { SampleFilter } from './sample-filter';
 import { SampleTable } from './sample-table';
 
+// Constants
+const TABS = [
+  { id: 0, label: 'table' },
+  { id: 1, label: 'counter' },
+] as const;
+
+const tabIds = TABS.map(({ id }) => id);
+
 export const Sample = () => {
-  const [isTable, setIsTable] = useState(true);
+  const [tabId, setTab] = useState(0);
 
   // "react-i18next"
   const { t } = useTranslation();
 
   // Methods
-  const handleChange = (_: MouseEvent<HTMLElement>, newIsTable: boolean) =>
-    setIsTable(newIsTable);
+  const handleChange = (_: SyntheticEvent, newTab: number) => setTab(newTab);
+  const getTabDisplay = (id: number) => (id !== tabId ? 'none' : '');
 
   return (
-    <>
-      <ToggleButtonGroup
-        fullWidth
-        exclusive
-        value={isTable}
-        arial-label="sample view"
-        onChange={handleChange}
-      >
-        <ToggleButton value={true}>{t('table')}</ToggleButton>
-        <ToggleButton value={false}>{t('counter')}</ToggleButton>
-      </ToggleButtonGroup>
+    <SampleProvider>
+      <Stack sx={{ minWidth: 650 }} spacing={2}>
+        <Tabs value={tabId} onChange={handleChange}>
+          {TABS.map(({ id, label }) => (
+            <Tab key={id} id={String(id)} label={t(label)} />
+          ))}
+        </Tabs>
 
-      <SampleProvider>
-        <Stack sx={{ minWidth: 650 }} spacing={1}>
-          {isTable ? (
-            <>
-              <SampleCreateTrigger />
-              <SampleFilter />
-              <SampleTable />
-            </>
-          ) : (
-            <SampleCounter />
-          )}
+        <Stack sx={{ display: getTabDisplay(tabIds[0]) }}>
+          <SampleTable />
         </Stack>
-      </SampleProvider>
-    </>
+
+        <Box sx={{ display: getTabDisplay(tabIds[1]) }}>
+          <SampleCounter />
+        </Box>
+      </Stack>
+    </SampleProvider>
   );
 };
