01c92f59c4a8f55b2ba00bb0eff7c0059fabe90c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-22 18:55:12 -0300

    chore: test

--- apps/web/src/features/sample/components/sample-table/sample-table-pagination.tsx
+++ apps/web/src/features/sample/components/sample-table/sample-table-pagination.tsx
@@ -5,7 +5,7 @@ import {
 import TablePagination from '@mui/material/TablePagination';
 import { ChangeEvent, MouseEvent } from 'react';
 
-export const SamplePagination = () => {
+export const SampleTablePagination = () => {
   const { rowsPerPageOptions, dataCount, rowsPerPage, page } =
     useReadSampleContext();
   const { changePage, changeRowsPerPage } = useUpdateSampleContext();
