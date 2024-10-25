53454fd926fb9dd06db7c82c0aa7c09839528b25
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-25 19:58:33 -0300

    chore: test

---
+++ apps/web/src/features/sample/hooks/use-trpc-sample-mutation.ts
@@ -0,0 +1,28 @@
+import { trpc } from '@/libs/trpc/hooks';
+
+export const useTrpcSampleMutation = () => {
+  // "trpc"
+  const utils = trpc.useUtils();
+  const getAllInvalidate = utils.sample.getAll.invalidate;
+
+  // Mutations
+  const createSample = trpc.sample.create.useMutation({
+    onSuccess() {
+      getAllInvalidate();
+    },
+  });
+
+  const updateSample = trpc.sample.update.useMutation({
+    onSuccess() {
+      getAllInvalidate();
+    },
+  });
+
+  const deleteSample = trpc.sample.delete.useMutation({
+    onSuccess() {
+      getAllInvalidate();
+    },
+  });
+
+  return { createSample, updateSample, deleteSample };
+};
