a6a9377c563709a5ba7203e840979470a2e1e15d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-26 11:37:49 -0300

    chore: test

---
+++ apps/web/src/features/sample/hooks/use-trpc-sample-get-all.ts
@@ -0,0 +1,23 @@
+import { trpc } from '@/libs/trpc/hooks';
+import { useSampleTableStore } from '@/libs/zustand/stores';
+
+export const useTrpcSampleGetAll = () => {
+  // "zustand"
+  const page = useSampleTableStore((state) => state.page);
+  const rowsPerPage = useSampleTableStore((state) => state.rowsPerPage);
+  const filters = useSampleTableStore((state) => state.filters);
+  const sortBy = useSampleTableStore((state) => state.sortBy);
+
+  // "trpc"
+  const { data, isFetching } = trpc.sample.getAll.useQuery({
+    page,
+    rowsPerPage,
+    filters,
+    sortBy,
+  });
+
+  return {
+    data,
+    isFetching,
+  };
+};
