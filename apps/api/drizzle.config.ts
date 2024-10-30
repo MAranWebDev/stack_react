ecf440e317dac68e9248aadc7225fb72f11dee40
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-30 15:53:01 -0300

    chore: test

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
+
+// "drizzle.config" is not in "tsconfig" so it can't use absolute import
 import {
   MIGRATIONS_FOLDER,
   SCHEMAS_FOLDER,
@@ -6,8 +8,8 @@ import {
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: SCHEMAS_FOLDER, // For drizzle generate
-  out: MIGRATIONS_FOLDER, // For drizzle generate
+  schema: SCHEMAS_FOLDER, // For drizzle generate only
+  out: MIGRATIONS_FOLDER, // For drizzle generate only
   verbose: true, // Print all queries that will be executed
   strict: true, // Always ask for an approve before pushing the schema
 });
