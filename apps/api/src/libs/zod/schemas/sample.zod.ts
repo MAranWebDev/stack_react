40d03ab7bbf8eb4b045f357baaeebb564655140c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2024-10-30 17:56:34 -0300

    chore: test

--- apps/api/src/libs/zod/schemas/sample.zod.ts
+++ apps/api/src/libs/zod/schemas/sample.zod.ts
@@ -6,9 +6,11 @@ const COLUMNS = ['id', 'name', 'isDone'] as const;
 // Base schemas
 const page = z.number().nonnegative();
 const rowsPerPage = z.number().positive().lte(25);
+
 const columnName = z.enum(COLUMNS);
 const isDesc = z.boolean();
 const sortBy = z.object({ columnName, isDesc });
+
 const id = z.string().trim().max(60);
 const name = z.string().trim().min(2).max(20);
 const isDone = z.boolean();
